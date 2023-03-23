7ddd3bdf1b8d4bd9647dbee9cb656244849008c9
Parker Parham <parkerparham@gmail.com>
2023-03-23 15:59:15 -0400

    Adds notes on how to send specific params of json data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express();
 const { products } = require('./data')
+
 app.get('/', (req, res) => {
-    res.json(products)
+    res.send('<h1>Home</h1><a href="/api/products">products</a>')
 })
 
-
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000...');
