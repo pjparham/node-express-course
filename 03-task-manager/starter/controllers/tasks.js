40c3d274642051eeec6a772e7be694c0e600debc
Parker Parham <parkerparham@gmail.com>
2023-06-06 16:56:35 -0400

    Adds basic setup for all task routes

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,9 +1,27 @@
-
-
 const getAllTasks = (req, res) =>{
     res.send('all items')
 }
 
+const createTask = (req, res) =>{
+    res.send('new item created')
+}
+
+const getTask = (req, res) =>{
+    res.send('get one task')
+}
+
+const updateTask = (req, res) =>{
+    res.send('update task')
+}
+
+const deleteTask = (req, res) =>{
+    res.send('delete task')
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
