15c0078e6ed571040d1172ccef9c61530db5068e
Parker Parham <parkerparham@gmail.com>
2023-06-16 13:55:57 -0400

    Adds functionality in controller for update task

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -31,12 +31,33 @@ const getTask = async (req, res) =>{
     }
 }
 
-const updateTask = (req, res) =>{
-    res.send('update task')
+const updateTask = async (req, res) =>{
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new:true,
+            runValidators:true,
+        })
+        if (task === null){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
-const deleteTask = (req, res) =>{
-    res.send('delete task')
+const deleteTask = async (req, res) =>{
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id: taskID})
+        if (task === null){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 module.exports = {
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -11,5 +11,4 @@ const {
 
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
 module.exports = router
