9fd4e9d977a7584825e1958c1a09fd340e2aef25
Parker Parham <parkerparham@gmail.com>
2023-06-21 12:37:28 -0400

    Adds basic setup for products router, products controller, and db connection

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,6 +5,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -16,6 +19,8 @@ app.get('/', (req,res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 // products route
 
 app.use(notFoundMiddleware)
@@ -26,6 +31,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         //connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)
